ab469c2d728192804d235beb3212b7359ef55d31
Sampreet Vaidya <93003875+sampreetucalgary07@users.noreply.github.com>
2025-01-10 00:52:44 -0700

    updated client

--- src/mongodb.py
+++ src/mongodb.py
@@ -10,21 +10,28 @@ import pandas as pd
 
 # env_variables = load_json("./env_variable.json")
 
+# [mongo]
+# host = "job-tracker-shard-00-01.otx6s.mongodb.net"
+# port = 27017
+# username = "sampreet-vaidya"
+# password = "fwNSIRqryuVNMLgz"
+
+
 
 # ---------- LOCAL CONNECTION ----------
 # username = os.environ["db_username"] #env_variables["db_username"]
 # password = os.environ[ "db_password"]#env_variables["db_password"]
 # uri = "mongodb+srv://"+ username + ":" + password + "@"+db_name+".otx6s.mongodb.net/?retryWrites=true&w=majority&appName=Job-tracker"
 # client = MongoClient(uri, tlsCAFile=certifi.where())
-
+client = MongoClient(f"mongodb+srv://sampreet-vaidya{st.secrets['MONGODB_PASSWORD']}@job-tracker.otx6s.mongodb.net/?retryWrites=true&w=majority&appName=<AppName>")
 
 # ---------- Streamlit Cloud Connection ----------
 # Create a new client and connect to the server
-@st.cache_resource
-def init_connection():
-    return MongoClient(**st.secrets["mongo"])
+# @st.cache_resource
+# def init_connection():
+#     return MongoClient(**st.secrets["mongo"])
 
-client = init_connection()
+# client = init_connection()
 
 db_name = os.environ["db_name"]#env_variables["db_name"]
 collection_name = os.environ["collection_name"]#env_variables["collection_name"]
