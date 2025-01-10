ee6845b00520a95b9de3d8285f1a80e7c13b1c32
Sampreet Vaidya <93003875+sampreetucalgary07@users.noreply.github.com>
2025-01-09 19:54:28 -0700

    removed env variables

--- src/init_mongodb.py
+++ src/init_mongodb.py
@@ -2,15 +2,14 @@ from pymongo.mongo_client import MongoClient
 import os
 import certifi
 import pandas as pd
-from utils import load_json
+#from utils import load_json
 
-env_variables = load_json("./env_variable.json")
+#env_variables = load_json("./env_variable.json")
 
-username = env_variables["db_username"]
-password = env_variables["db_password"]
-db_name = env_variables["db_name"]
-collection_name = env_variables["collection_name"]
-
+username = os.environ["db_username"] #env_variables["db_username"]
+password = os.environ[ "db_password"]#env_variables["db_password"]
+db_name = os.environ["db_name"]#env_variables["db_name"]
+collection_name = os.environ["collection_name"]#env_variables["collection_name"]
 uri = "mongodb+srv://"+ username + ":" + password + "@"+db_name+".otx6s.mongodb.net/?retryWrites=true&w=majority&appName=Job-tracker"
 
 # Create a new client and connect to the server
