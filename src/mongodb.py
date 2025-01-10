5a2b9cb1f903c08f0fdc812be9edd98c30e00e15
Sampreet Vaidya <93003875+sampreetucalgary07@users.noreply.github.com>
2025-01-09 20:20:30 -0700

    added streamlit compatibles

--- src/mongodb.py
+++ src/mongodb.py
@@ -4,26 +4,34 @@ import certifi
 import os
 import streamlit as st
 import pandas as pd
-from pymongo.server_api import ServerApi
+# from pymongo.server_api import ServerApi
 #from src.utils import load_json
 
 
 # env_variables = load_json("./env_variable.json")
 
-username = os.environ["db_username"] #env_variables["db_username"]
-password = os.environ[ "db_password"]#env_variables["db_password"]
+
+# ---------- LOCAL CONNECTION ----------
+# username = os.environ["db_username"] #env_variables["db_username"]
+# password = os.environ[ "db_password"]#env_variables["db_password"]
+# uri = "mongodb+srv://"+ username + ":" + password + "@"+db_name+".otx6s.mongodb.net/?retryWrites=true&w=majority&appName=Job-tracker"
+# client = MongoClient(uri, tlsCAFile=certifi.where())
+
+
+# ---------- Streamlit Cloud Connection ----------
+# Create a new client and connect to the server
+@st.cache_resource
+def init_connection():
+    return MongoClient(**st.secrets["mongo"])
+
+client = init_connection()
+
 db_name = os.environ["db_name"]#env_variables["db_name"]
 collection_name = os.environ["collection_name"]#env_variables["collection_name"]
-uri = "mongodb+srv://"+ username + ":" + password + "@"+db_name+".otx6s.mongodb.net/?retryWrites=true&w=majority&appName=Job-tracker"
-
-# Create a new client and connect to the server
-
 
 #@st.cache_resource
 def update_mongodb_db(df):
     df.fillna("2025-01-09",inplace=True)
-    #client = MongoClient(uri, tlsCAFile=certifi.where())
-    client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
     collection.delete_many({})
@@ -34,10 +42,8 @@ def update_mongodb_db(df):
         st.write(e)
     
     
-#@st.cache_resource (ttl=600)
+@st.cache_resource (ttl=200)
 def read_mongodb_db():
-    
-    client = MongoClient(uri, tlsCAFile=certifi.where())
     db = client[db_name]
     collection = db[collection_name]
     items = collection.find()
