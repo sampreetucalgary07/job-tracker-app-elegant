1ea54a54a3bcb4693f1612bd338d03a55e3d755d
Sampreet Vaidya <93003875+sampreetucalgary07@users.noreply.github.com>
2025-01-09 19:38:46 -0700

    add passcode to env variable.json

--- src/sections.py
+++ src/sections.py
@@ -2,12 +2,16 @@ import streamlit as st
 import time
 import os
 
+from src.utils import load_json
+
+env_variables = load_json("./env_variable.json")
+
 @st.dialog(title="Welcome to Job tracker app")
 def passcode_dialog():
     st.session_state.passcode = st.text_input("Enter passcode", type="password", max_chars=6)
 
     if st.button("Submit", type="primary", icon = "🎯"):
-        if st.session_state.passcode == os.environ['app_passcode']:
+        if st.session_state.passcode == env_variables["app_passcode"]:
             #st.success("Correct passcode. Welcome to the app!")
             with st.spinner('Welcome! Loading job applications...'):
                 time.sleep(1)
